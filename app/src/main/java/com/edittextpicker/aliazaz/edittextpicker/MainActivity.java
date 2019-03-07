c660bd2feeeeba0a6802f6a6962c6b93b1374363
ali.azaz <ali.azaz.alam@hotmail.com>
2019-03-07 18:38:57 +0500

    commit

--- app/src/main/java/com/edittextpicker/aliazaz/edittextpicker/MainActivity.java
+++ app/src/main/java/com/edittextpicker/aliazaz/edittextpicker/MainActivity.java
@@ -2,13 +2,42 @@ package com.edittextpicker.aliazaz.edittextpicker;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.edittextpicker.aliazaz.textpicker.TextPicker;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnSubmit;
+    EditText txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initializingComponents();
+        settingListeners();
+
+    }
+
+    private void initializingComponents() {
+        btnSubmit = findViewById(R.id.btnSubmit);
+        txt = findViewById(R.id.txtMask);
+    }
+
+    private void settingListeners() {
+
+        ((TextPicker) txt).isEmptyTextBox();
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
 
 
     }
